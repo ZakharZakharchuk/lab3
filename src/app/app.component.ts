e47995a18abdba0c6d83177479ad8594bdd87928
Zakhar Zakharchuk <zakhar.zakharchuk3@gmail.com>
2024-11-14 22:54:27 +0200

    Pipes1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,40 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <child-comp [name]="name"></child-comp>
-    <input type="text" [(ngModel)]="name"/>`
+    <div>Без форматування: {{myDate}}</div>
+    <div>З форматуванням: {{myDate | date}}</div>
+    <div>{{welcome | uppercase}}</div>
+    <div>{{welcome | lowercase}}</div>
+    <div>{{persentage | percent}}</div>
+    <div>{{persentage | currency}}</div>
+    <div>{{welcome | slice:3}}</div>
+    <div>{{welcome | slice:6:11}}</div>
+    <div>{{pi | number:'2.1-2'}}</div>
+    <div>{{pi | number:'3.5-5'}}</div>
+    <div>{{money | currency:'UAH':'code'}}</div>
+    <div>{{money | currency:'UAH':'symbol-narrow'}}</div>
+    <div>{{money | currency:'UAH':'symbol':'1.1-1'}}</div>
+    <div>{{money | currency:'UAH':'symbol-narrow':'1.1-1'}}</div>
+    <div>{{money | currency:'UAH':'Тільки сьогодні по ціні '}}</div>
+    <div>{{message | slice:6:11 | uppercase}}</div>
+    <div>Число до форматування: {{x}}<br>Число після форматування: {{x |
+        format}}</div>
+    <hr/>
+    <div>{{users | join}}</div>
+    <div>{{users | join:1}}</div>
+    <div>{{users | join:1:3}}</div>
+    <hr>
+    <div>Число до квадратного кореня: {{x}}</div>
+    <div>Число після квадратного кореня: {{x | sqrt}}</div>
+  `
 })
 export class AppComponent {
-  name: string = "Tom";
+  myDate = new Date(1961, 3, 12);
+  welcome: string = "Hello World!";
+  persentage: number = 0.14;
+  pi: number = 3.1415;
+  money: number = 23.45;
+  message = "Hello World!";
+  x: number = 15.45;
+  users = ["Tom", "Alice", "Sam", "Kate", "Bob"];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import {ChildComponent} from './child.component';
+import {FormatPipe} from './format.pipe';
+import {JoinPipe} from './join.pipe';
+import {SqrtPipe} from './sqrt.pipe';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ChildComponent],
+  imports: [BrowserModule],
+  declarations: [AppComponent, FormatPipe, JoinPipe, SqrtPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule {
